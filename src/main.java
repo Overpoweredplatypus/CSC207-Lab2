38324e17848c93e94abcfe4063ddbb80f781f646
Minseok Kwag <havefun01@v1042-wn-rt-b-58-210.campus-dynamic.uwaterloo.ca>
2024-05-26 11:53:52 -0400

    TASK 2 DONE

--- src/main.java
+++ src/main.java
@@ -1,2 +1,11 @@
-package PACKAGE_NAME;public class Main {
+import java.util.ArrayList;
+import java.util.List;
+
+public class main {
+    public void largeSumTest() {
+
+    }
+
+    private void assertEquals(String s, long x, long sum) {
+    }
 }
